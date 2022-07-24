e80a198dda093e252f1ae967c6892b1290740833
Nikolai Seip <nikolai.seip@gmail.com>
2022-07-24 20:02:50 +0200

    test: add first testcase for filtering outdated images

---
+++ filter_images_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestFilterImages(t *testing.T) {
+	testImages := make(map[string]ImageData)
+	testImages["my.domain.com/outdated:1.0.0"] = ImageData{
+		Image:          "my.domain.com/outdated:1.0.0",
+		BuildTimestamp: time.Now().Add(-(time.Hour * 24 * 40)),
+	}
+	testImages["my.domain.com/ok:1.0.0"] = ImageData{
+		Image:          "my.domain.com/ok:1.0.0",
+		BuildTimestamp: time.Now().Add(-(time.Hour * 24 * 20)),
+	}
+	timestamp := time.Now().Add(-(time.Hour * 24 * 30))
+
+	filterOutdatedImages(&testImages, timestamp)
+
+	if len(testImages) != 1 {
+		t.Fatalf("Expected %d outdated image but got %d", 1, len(testImages))
+	}
+
+}
