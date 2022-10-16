3ad828463a9422e3392505a74770d9f3d7e06577
Nikolai Seip <nikolai.seip@gmail.com>
2022-10-16 11:38:17 +0200

    fix e2e checks

--- test/test_check.sh
+++ test/test_check.sh
@@ -1,6 +1,9 @@
 cat ./result.json
 
-
+echo ""
+echo ""
+echo "Checking result ..."
+echo ""
 if [[ $(jq '. | length' result.json) != 1 ]]; then
   echo "Number of results not equals 1"; exit 1;
 fi
@@ -9,6 +12,7 @@ if [[ $(jq '.["nginx:1.14.2"].Findings | length' result.json) != 3 ]]; then
   echo "Number of Findings for \"nginx:1.14.2\" not equals 3"; exit 1;
 fi
 
-if [[ $(jq '.["nginx:1.14.2"].Findings[0].NotificationData.Email' result.json) != "ns-admin@org.com" ]]; then
+if [[ "$(jq -r '.["nginx:1.14.2"].Findings[0].NotificationData.Email' result.json)" != "ns-admin@org.com" ]]; then
   echo "First finding for \"nginx:1.14.2\": Email value not expected"; exit 1;
 fi
+echo "Finished! All ok ..."
